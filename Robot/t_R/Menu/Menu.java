fb0e1d70b038455ea53c8dd6fc3c8ad0287c8162
unniooo <120465027+unniooo@users.noreply.github.com>
2023-01-14 14:43:35 +0800

    Add files via upload

---
+++ Robot/t_R/Menu/Menu.java
@@ -0,0 +1,14 @@
+package Menu;
+
+public class Menu {
+    public void menuOne(){
+        System.out.println("*******************************");
+        System.out.println("请选择清洁模式：");
+        System.out.println("           1:吸尘");
+        System.out.println("           2:扫地、拖地");
+        System.out.println("清洁强度分为两档：高（3）、低（4）");
+        System.out.println("终止打扫请输入0");
+        System.out.println("*******************************");
+    }
+
+}
---
+++ Robot/t_R/robot/Robot.java
@@ -0,0 +1,23 @@
+package robot;
+
+public class Robot {
+// xichen;//功能一：吸尘
+// saodi;//功能二：1.扫地2.拖地
+// honggan;//烘干
+
+
+
+    public void Xichen(){
+        System.out.println("启动第一个功能(默认低强度)");
+
+//        this.xichen = this.xichen + 200.0f;
+    }
+    public void saodtuodi(){
+        System.out.println("启动第二个功能(默认低强度)");
+    }
+    public void honggan(){
+        System.out.println("终止打扫，进入自动清洁、烘干");
+    }
+
+
+}
