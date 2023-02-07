1a61972c0361d5901796419a901d68f54afb5b74
unniooo <120465027+unniooo@users.noreply.github.com>
2023-02-07 23:51:55 +0800

    Add files via upload

---
+++ Demo/MethodDemo1.java
@@ -0,0 +1,14 @@
+public class MethodDemo1 {
+    public static void main(String[] args) {
+        //目标：掌握最简单的方法定义和调用的格式
+
+        //调用方法
+        playGame();
+
+    }
+    //定义一个方法
+    public static void playGame(){
+        System.out.println("选");
+        System.out.println("开始准备");
+    }
+}
---
+++ Demo/MethodDemo2.java
@@ -0,0 +1,10 @@
+public class MethodDemo2 {
+    public static void main(String[] args) {
+        printGFInfo();
+    }
+
+    public static void printGFInfo(){
+        System.out.println("lf");
+        System.out.println("18岁");
+    }
+}
