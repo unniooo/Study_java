620aaeee54f8f2ab232c1b1805fa75767e240e49
unniooo <120465027+unniooo@users.noreply.github.com>
2023-02-07 23:55:58 +0800

    Add files via upload

---
+++ Demo/MethodDemo3.java
@@ -0,0 +1,13 @@
+public class MethodDemo3 {
+    public static void main(String[] args) {
+        getSum();
+    }
+
+    public static void getSum(){
+        int num1 = 10;
+        int num2 = 20;
+
+        int result = num1 + num2;
+        System.out.println(result);
+    }
+}
