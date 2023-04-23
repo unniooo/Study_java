0696b6a18365263d253a5e2145c81be144985d7c
unniooo <120465027+unniooo@users.noreply.github.com>
2023-04-23 23:51:56 +0800

    Add files via upload

---
+++ Test02/src/Test3_11/Boyfriends.java
@@ -0,0 +1,16 @@
+package Test3_11;
+/**
+ * 定义数组存储4个男朋友的对象
+ * 属性： 姓名，年龄，性别，爱好
+ * 要求： 计算除=出4个男朋友的平均年龄
+ * 要求： 统计年龄比平均值低的男朋友有几个，并把他们所有的信息打印出来
+ * */
+public class Boyfriends {
+    private String name;
+    private int age;
+    private String gender;
+    private String hobby;
+
+
+
+}
---
+++ Test02/src/apidemo/ScannerDemo1.java
@@ -0,0 +1,18 @@
+package apidemo;
+
+import java.util.Scanner;
+
+public class ScannerDemo1 {
+    public static void main(String[] args) {
+        //1.创建对象
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入一个小数：");
+        //2.接收一个小数
+        double result = sc.nextDouble();  // Ctrl + Alt + v
+        //3.输出打印
+        System.out.println(result);
+
+
+
+    }
+}
